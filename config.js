49bb2f19d87ab64a5139b08a9199c28a94c993f4
unknown <mabg0630@gmail.com>
2024-09-05 08:52:46 -0500

    pre

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
 const CONFIG = {
-    HOST: 'http://127.0.0.1:5000'
+    // HOST: 'http://127.0.0.1:5000'
+    HOST: 'https://ai.tickapp.online/'
 };
 
 export default CONFIG;
--- fetchvideo.js
+++ fetchvideo.js
@@ -1,50 +1,3 @@
-import CONFIG from './config.js';
-
-async function fetchVideos() {
-    const formData = new FormData(document.getElementById('fetchForm'));
-    const word = formData.get('word');
-
-    await fetch(`${CONFIG.HOST}/get_video`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ word: word })
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return response.json().then(err => { throw err; });
-        }
-    })
-    .then(data => {
-        const responseDiv = document.getElementById('response');
-        responseDiv.innerHTML = ''; // Limpia el contenido anterior
-        const video = data;
-        console.log(video);
-            if (video.mime_type === 'video/mp4') {
-                const videoPlayer = document.createElement('video');
-                videoPlayer.width = 320;
-                videoPlayer.height = 240;
-                videoPlayer.controls = true;
-                videoPlayer.src = `data:${video.mime_type};base64,${video.video_base64}`;
-                responseDiv.appendChild(videoPlayer);
-            } else if (video.mime_type === 'image/gif') {
-                const gifImage = document.createElement('img');
-                gifImage.width = 320;
-                gifImage.height = 240;
-                gifImage.src = `data:${video.mime_type};base64,${video.video_base64}`;
-                responseDiv.appendChild(gifImage);
-            }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        document.getElementById('response').innerText = 'Error: ' + JSON.stringify(error);
-    });
-}
-
-// fetchvideo.js
 // import CONFIG from './config.js';
 
 // async function fetchVideos() {
@@ -70,23 +23,70 @@ async function fetchVideos() {
 //         responseDiv.innerHTML = ''; // Limpia el contenido anterior
 //         const video = data;
 //         console.log(video);
-//         if (video.mime_type === 'video/mp4') {
-//             const videoPlayer = document.createElement('video');
-//             videoPlayer.width = 320;
-//             videoPlayer.height = 240;
-//             videoPlayer.controls = true;
-//             videoPlayer.src = `data:${video.mime_type};base64,${video.video_base64}`;
-//             responseDiv.appendChild(videoPlayer);
-//         } else if (video.mime_type === 'image/gif') {
-//             const gifImage = document.createElement('img');
-//             gifImage.width = 320;
-//             gifImage.height = 240;
-//             gifImage.src = `data:${video.mime_type};base64,${video.video_base64}`;
-//             responseDiv.appendChild(gifImage);
-//         }
+//             if (video.mime_type === 'video/mp4') {
+//                 const videoPlayer = document.createElement('video');
+//                 videoPlayer.width = 320;
+//                 videoPlayer.height = 240;
+//                 videoPlayer.controls = true;
+//                 videoPlayer.src = `data:${video.mime_type};base64,${video.video_base64}`;
+//                 responseDiv.appendChild(videoPlayer);
+//             } else if (video.mime_type === 'image/gif') {
+//                 const gifImage = document.createElement('img');
+//                 gifImage.width = 320;
+//                 gifImage.height = 240;
+//                 gifImage.src = `data:${video.mime_type};base64,${video.video_base64}`;
+//                 responseDiv.appendChild(gifImage);
+//             }
 //     })
 //     .catch(error => {
 //         console.error('Error:', error);
 //         document.getElementById('response').innerText = 'Error: ' + JSON.stringify(error);
 //     });
 // }
+
+// fetchvideo.js
+import CONFIG from './config.js';
+
+async function fetchVideos() {
+    const formData = new FormData(document.getElementById('fetchForm'));
+    const word = formData.get('word');
+
+    await fetch(`${CONFIG.HOST}get_video`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ word: word })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return response.json().then(err => { throw err; });
+        }
+    })
+    .then(data => {
+        const responseDiv = document.getElementById('response');
+        responseDiv.innerHTML = ''; // Limpia el contenido anterior
+        const video = data;
+        console.log(video);
+        if (video.mime_type === 'video/mp4') {
+            const videoPlayer = document.createElement('video');
+            videoPlayer.width = 320;
+            videoPlayer.height = 240;
+            videoPlayer.controls = true;
+            videoPlayer.src = `data:${video.mime_type};base64,${video.video_base64}`;
+            responseDiv.appendChild(videoPlayer);
+        } else if (video.mime_type === 'image/gif') {
+            const gifImage = document.createElement('img');
+            gifImage.width = 320;
+            gifImage.height = 240;
+            gifImage.src = `data:${video.mime_type};base64,${video.video_base64}`;
+            responseDiv.appendChild(gifImage);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('response').innerText = 'Error: ' + JSON.stringify(error);
+    });
+}
