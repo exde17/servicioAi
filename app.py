c9c56eb8c28ceaaf5ec3afb6b5b71f150ddb4640
unknown <mabg0630@gmail.com>
2024-10-30 08:39:13 -0500

    omitir tildes

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import base64
 import os
 import subprocess
 import speech_recognition as sr
+import unicodedata
+import re
 from evaluate_service import evaluate_model, load_lstm_model
 from constants import ROOT_PATH, FRAME_ACTIONS_PATH, DATA_PATH
 from flask_cors import CORS
@@ -26,10 +28,10 @@ def evaluate():
     try:
         # Obtener parámetros del request
         threshold = request.json.get('threshold', 0.9)
-        video_base64 = request.json.get('video_base64')
+        videos = request.json.get('videos')
 
-        # Decodificar el video base64
-        video_data = base64.b64decode(video_base64)
+        # Decodificar el videos base64
+        video_data = base64.b64decode(videos)
         video_path = "temp_video.mp4"
         with open(video_path, "wb") as video_file:
             video_file.write(video_data)
@@ -110,49 +112,58 @@ CONNECTORS = (
     "Con el fin de"
 )
 
-
 @app.route('/get_video', methods=['POST'])
 def get_video():
     try:
-        phrase = request.json.get('word')
-        if not phrase:
-            return jsonify({"error": "No word provided"}), 400
+        # Obtener la palabra o frase desde la solicitud
+        frase = request.json.get('word')
+        if not frase:
+            return jsonify({"error": "No se proporcionó una palabra"}), 400
 
-        words = phrase.split()  # Divide la frase en palabras individuales
-        video_clips = []
+        # Normalizar y limpiar la frase eliminando tildes y signos de puntuación
+        frase = unicodedata.normalize('NFKD', frase).encode('ASCII', 'ignore').decode('utf-8')
+        frase = re.sub(r'[^\w\s]', '', frase)  # Eliminar puntuación
+        
+        # Dividir la frase en palabras y procesarlas una por una
+        palabras = frase.lower().split()
+        clips_video = []
 
-        for word in words:
-            if word.lower() in CONNECTORS:  # Omitir conectores
+        for palabra in palabras:
+            if palabra in CONNECTORS:  # Omitir conectores
                 continue
 
-            video_path_mp4 = os.path.join("static", "videos", f"{word}.mp4")
-            video_path_gif = os.path.join("static", "videos", f"{word}.gif")
+            # Comprobar si existe un archivo mp4 o gif para la palabra
+            ruta_video_mp4 = os.path.join("static", "videos", f"{palabra}.mp4")
+            ruta_video_gif = os.path.join("static", "videos", f"{palabra}.gif")
 
-            if os.path.isfile(video_path_mp4):
-                video_clips.append(VideoFileClip(video_path_mp4))
-            elif os.path.isfile(video_path_gif):
-                video_clips.append(VideoFileClip(video_path_gif))
+            if os.path.isfile(ruta_video_mp4):
+                clips_video.append(VideoFileClip(ruta_video_mp4))
+            elif os.path.isfile(ruta_video_gif):
+                clips_video.append(VideoFileClip(ruta_video_gif))
             else:
-                continue  # Omite si no se encuentra un video o GIF para esta palabra
+                continue  # Omitir si no se encuentra un videos o GIF para esta palabra
 
-        if video_clips:
-            final_clip = concatenate_videoclips(video_clips)
+        # Combinar clips de videos si están disponibles
+        if clips_video:
+            clip_final = concatenate_videoclips(clips_video)
+            ruta_salida = "static/videos/video_final.mp4"
+            clip_final.write_videofile(ruta_salida, codec="libx264")
 
-            output_path = "static/videos/final_video.mp4"
-            final_clip.write_videofile(output_path, codec="libx264")
-
-            with open(output_path, "rb") as video_file:
-                video_base64 = base64.b64encode(video_file.read()).decode('utf-8')
+            # Codificar el videos final en base64
+            with open(ruta_salida, "rb") as archivo_video:
+                videos = base64.b64encode(archivo_video.read()).decode('utf-8')
             
-            # Elimina el archivo temporal si ya no lo necesitas
-            #os.remove(output_path)
+            # Eliminar el archivo temporal si ya no es necesario
+            # os.remove(ruta_salida)
         else:
-            video_base64 = None
+            videos = None
 
-        if not video_base64:
-            return jsonify({"error": "No videos found for any words"}), 404
+        # Devolver error si no se encuentran videos
+        if not videos:
+            return jsonify({"error": "No se encontraron videos para ninguna palabra"}), 404
 
-        return jsonify({"videos": video_base64}), 200
+        # Retornar el videos final en formato base64
+        return jsonify({"videos": videos, "mime_type": "videos/mp4"}), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
